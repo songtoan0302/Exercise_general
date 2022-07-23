ec41fc82dca89030e869d63c0ae2c40a11f3c621
songtoan0302 <nguyensongtoan0302@gmail.com>
2022-07-23 20:38:08 +0700

    create project greeting, build api get and build functions test for api that. Configuration port for server

---
+++ greeting/src/main/java/org/aibles/greeting/controller/HelloController.java
@@ -0,0 +1,18 @@
+package org.aibles.greeting.controller;
+
+/**
+ * @author ToanNS
+ */
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+  @GetMapping("/")
+  public String index() {
+    return "Greetings from Spring Boot!";
+  }
+
+}
---
+++ greeting/src/test/java/org/aibles/greeting/GreetingApplicationTests.java
@@ -0,0 +1,11 @@
+package org.aibles.greeting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreetingApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
